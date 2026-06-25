dda90533c239ced47c7f3c015834151937b21bba
1f10250297 <s1f102502979@iniad.org>
2026-06-25 16:41:56 +0900

    修正２

--- todo/models.py
+++ todo/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 
 class Task(models.Model):
-    title = models.CharField(max_length = 100)
-    completed =  models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
 
--- todo/tests.py
+++ todo/tests.py
@@ -75,11 +75,11 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response.context['tasks']), 1)
 
     def test_index_get_order_post(self):
-        task1 = Task(title = 'task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
-        task2 = Task(title = 'task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
-        client=Client()
+        client = Client()
         response = client.get('/?order=post')
 
         self.assertEqual(response.status_code, 200)
@@ -92,7 +92,7 @@ class TodoViewTestCase(TestCase):
         task1.save()
         task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
         task2.save()
-        client=Client()
+        client = Client()
         response = client.get('/?order=due')
 
         self.assertEqual(response.status_code, 200)
